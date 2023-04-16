6d54c84afd5be6e5f4c01780e3e572f0ea80d287
John Knott <john.knott@gmail.com>
2023-04-16 09:40:36 +0100

    Added a page and link prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [prefetch()]
+});
